99b9cbf0dd76a2f82279553ec32ee5a80d64fe40
BorysCzapski <113203258+BorysCzapski@users.noreply.github.com>
2024-03-25 18:19:57 +0100

    Add Rotate L and Rotate R methods

--- Main.java
+++ Main.java
@@ -29,6 +29,45 @@ class RBTree {
         this.rootNode = this.nullNode; //The root is now a null node (which is NIL), important note: the root color is black now
     }
 
+    public void rotateL(RBTreeNode x){ //Left rotate method, used when there are two consecutive red nodes, and second node is right child
+        //X represents existing node, higher in the tree, Y is the lower node that will swap places with X
+        RBTreeNode y = x.right; // Y is right child of X
+        x.right = y.left; // After the rotation Y's left child will be X right child
+        if (y.left != this.nullNode){ //Check if Y's left child is not null
+            y.left.parent = x; //Y's left child parent after rotation will be X
+        }
+        y.parent = x.parent; // X will swap places with Y
+        if (x.parent == this.nullNode){ //Check if X is the root
+            this.rootNode = y;
+        }
+        else if (x == x.parent.left){ //Check if the X is left or right child
+            x.parent.left = y;
+        }else{
+            x.parent.right = y;
+        }
+        y.left = x; //After rotation Y's left child is X
+        x.parent = y; //After rotation X's parent is Y
+    }
+    
+    public void rotateR(RBTreeNode x){ //Right rotate method, used when there are two consecutive red nodes, and second node is left
+        //X represents existing node, higher in the tree, Y is the lower node that will swap places with X
+        RBTreeNode y = x.left; // Y is left child of X
+        x.left = y.right; // After the rotation Y's right child will be X left child
+        if (y.right != this.nullNode){ //Check if Y's right child is not null
+            y.right.parent = x; // Y's right child parent after rotations is X
+        }
+        y.parent = x.parent; //For swapping places note: Line 68 - complete swap
+        if (x.parent == this.nullNode){ //Check if X is the root
+            this.rootNode = y;
+        } else if (x == x.parent.right){ // Check if X is the right or left child
+            x.parent.right = y;
+        } else {
+            x.parent.left = y;
+        }
+        y.right = x; // Y's right child after rotation is X
+        x.parent = y; // Swap places
+    }
+
 }
 
 
